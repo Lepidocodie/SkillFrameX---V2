40820fa5db2ac4fbe3d2c0f2d6f93b823ab6f34f
Bonchon2023 <sonjainarongrit15@gmail.com>
2026-03-16 21:54:21 +0700

    deploy bug fix and add .next

--- .next_dev_2/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__0132581b._.css",
-  "static/chunks/app_components_cb0a8a4f._.js",
-  "static/chunks/node_modules_a4e770f9._.js"
-],
-    source: "dynamic"
-});
