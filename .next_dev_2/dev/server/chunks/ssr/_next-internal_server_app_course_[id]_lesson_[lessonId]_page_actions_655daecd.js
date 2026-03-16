a85b897fb2cf4c970fee04f29e76bb0a2562f674
Lepidocodie <son.narongrit@gmail.com>
2026-03-16 21:38:35 +0700

    Delete .next_dev_2/dev directory

--- .next_dev_2/dev/server/chunks/ssr/_next-internal_server_app_course_[id]_lesson_[lessonId]_page_actions_655daecd.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/course/[id]/lesson/[lessonId]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_course_%5Bid%5D_lesson_%5BlessonId%5D_page_actions_655daecd.js.map
--- .next_dev_2/dev/static/chunks/app_course_[id]_page_tsx_3bd93824._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_752da8e8._.js",
-  "static/chunks/app_cf43d735._.js"
-],
-    source: "dynamic"
-});
